b0a0e1822f3820049543776953efb4f06bf6a9a8
andrew0416 <andrew0416@naver.com>
2025-07-11 16:13:34 +0900

    Update middleware.dto.ts

--- src/middlewares/middleware.dto.ts
+++ src/middlewares/middleware.dto.ts
@@ -13,7 +13,6 @@ export function validateDto(dtoClass: any, source: 'body' | 'params' | 'query' =
       const errorMessages = errors.flatMap(err =>
         Object.values(err.constraints || {})
       );
-      console.log("오류: 입력값 잘못됨")
       res.status(400).json({ message: '입력값이 잘못되었습니다.', errors: errorMessages });
       return;
     }
