e95c1c42509df2f3969fb5eb4d4fcc15e44e36d2
CreateWater <blackrp001@naver.com>
2025-07-16 14:08:51 +0900

    Co-authored-by: andrew0416 <andrew0416@naver.com>

--- build/src/routers/profile.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const profile_controller_1 = require("../controllers/profile.controller");
-const middleware_auth_1 = require("../middlewares/middleware.auth");
-const router = (0, express_1.Router)();
-router.get('/', middleware_auth_1.authMiddleware, profile_controller_1.getUserProfile);
-router.patch('/', middleware_auth_1.authMiddleware, profile_controller_1.updateUserProfile);
-exports.default = router;
